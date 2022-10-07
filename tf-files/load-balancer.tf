87f0c5eca5a0e048724708dce7b83452ff699b5b
Utkarsh Naik <utkarsh17naik@gmail.com>
2022-10-07 19:33:38 +0530

    Updated tf for alb sg

--- tf-files/load-balancer.tf
+++ tf-files/load-balancer.tf
@@ -20,3 +20,32 @@ resource "aws_lb" "alb" {
     Environment = var.environment
   }
 }
+
+resource "aws_security_group" "alb" {
+  name   = "${aws_lb.alb.name}-sg"
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    protocol         = "tcp"
+    from_port        = 80
+    to_port          = 80
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    protocol         = "tcp"
+    from_port        = 443
+    to_port          = 443
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    protocol         = "-1"
+    from_port        = 0
+    to_port          = 0
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
