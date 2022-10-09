388aa77645c648c98e3ccbd6521658fc69aace99
Utkarsh Naik <utkarsh17naik@gmail.com>
2022-10-10 00:31:53 +0530

    Added codebuild and output variables

--- rates/config.py
+++ rates/config.py
@@ -1,7 +1,7 @@
 DB = {
-    "name": "postgres",
-    "user": "postgres",
-    "host": "13.235.238.195",
-"password": "test123"
+    "name": "",
+    "user": "",
+    "host": "",
+"password": ""
 
 }
